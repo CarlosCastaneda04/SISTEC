ba1fe5d8694d55304e0f76bb9627adba5b30edf0
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2025-04-04 22:54:33 -0600

    componente de empleado tecnico

---
+++ SISTEC frontend/src/components/EmpleadoTecnico/EmpleadoTecnico.jsx
@@ -0,0 +1,12 @@
+
+
+const EmpleadoTecnico = () => {
+  return (
+    <div>
+      <h1>Categorias de las solicitudes</h1>
+      <p>Esta es la sección del empleado técnico.</p>
+    </div>
+  );
+}
+
+export default EmpleadoTecnico;
