760cadbbef15716afc617e1e8105e826b8985924
F7MH2R <skyicef7@gmail.com>
2025-04-05 09:20:26 -0600

    Squashed commit of the following:
    commit 03b90960a95e2530b37c5df3f866c78e19fa7fe8
    Author: Ivonne Duran <111211850+ivonne-duran@users.noreply.github.com>
    Date:   Sat Apr 5 08:26:06 2025 -0600
    
        Componentes EmpleadoTecnico y CategoriaSeleccionada (#2)
    
        * componenetes nuevos
    
        * Ajustes finales componentes
    
        ---------
    
        Co-authored-by: F7MH2R <skyicef7@gmail.com>

--- SISTEC frontend/src/router.jsx
+++ SISTEC frontend/src/router.jsx
@@ -6,28 +6,30 @@ import About from './components/About';
 import ServiciosTecnicos from './pages/ServiciosPage'; 
 import Diagnostico from './components/Diagnostico';
 import AgregarSolicitudPage from './pages/AgregarSolicitudPage';
-
+import EmpleadoTecnico from "./components/EmpleadoTecnico/EmpleadoTecnico";
+import CategoriaSeleccionada from "./components/CategoriaSeleccionada/CategoriaSeleccionada";
 
 
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />, 
+    path: "/",
+    element: <App />,
     children: [
       {
-        path: '/login',
-        element: <LoginPage />
+        path: "/login",
+        element: <LoginPage />,
       },
       {
-        path: '/acerca',
-        element: <About />
+        path: "/acerca",
+        element: <About />,
       },
       {
-        path: '/registro',
-        element: <RegisterPage />
+        path: "/registro",
+        element: <RegisterPage />,
       },
       {
+
         path: '/servicios-tecnicos',
         element: <ServiciosTecnicos />
       },
@@ -38,9 +40,17 @@ const router = createBrowserRouter([
       {
         path: '/agregar-solicitud',
         element: <AgregarSolicitudPage />
-      }      
-    ]
-  }
-]);
+      },
+      {
+        path: "/empleado-tecnico",
+        element: <EmpleadoTecnico />,
+      },
+      {
+        path: "/categoria-seleccionada",
+        element: <CategoriaSeleccionada />,
+      },
+    }
+  ]
+);
 
 export default router;
