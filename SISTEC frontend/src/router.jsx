d094590a246f93ee86fe0ce70d52616681d8b9a6
Ivonne Duran <111211850+ivonne-duran@users.noreply.github.com>
2025-04-08 22:57:29 -0600

    Pantallas trabajadas tecnico servicio y confirmacionExitosa (#4)
    Co-authored-by: AndyRoldan <69610526+AndyRoldan@users.noreply.github.com>

--- SISTEC frontend/src/router.jsx
+++ SISTEC frontend/src/router.jsx
@@ -1,14 +1,16 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from './App'; 
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import About from './components/About';
-import ServiciosTecnicos from './pages/ServiciosPage'; 
-import Diagnostico from './components/Diagnostico';
-import AgregarSolicitudPage from './pages/AgregarSolicitudPage';
-
-
-
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import About from "./components/About";
+import ServiciosTecnicos from "./pages/ServiciosPage"; // asegúrate de que esta ruta coincida
+import EmpleadoTecnico from "./components/EmpleadoTecnico/EmpleadoTecnico";
+import CategoriaSeleccionada from "./components/CategoriaSeleccionada/CategoriaSeleccionada"; 
+import TecnicosDisponibles from "./components/TecnicosDisponibles/tecnicosDisponibles";
+import AsignarServicio from "./components/Tecnicos/AsignarServicio"; // asegúrate de que esta ruta coincida 
+import AsignacionExitosa from "./components/Tecnicos/AsignacionExitosa"; // asegúrate de que esta ruta coincida
+import Diagnostico from "./components/Diagnostico";
+import AgregarSolicitudPage from "./pages/AgregarSolicitudPage";
 
 const router = createBrowserRouter([
   {
@@ -38,7 +40,19 @@ const router = createBrowserRouter([
       {
         path: '/agregar-solicitud',
         element: <AgregarSolicitudPage />
-      }      
+      },      
+      {
+        path: "/tecnicos-disponibles",
+        element: <TecnicosDisponibles />,
+      },
+      {
+        path: "/asignar-tecnicos",
+        element: < AsignarServicio/>,
+      },
+      {
+        path: "/asignarcion-confirmada",
+        element: < AsignacionExitosa/>,
+      },
     ]
   }
 ]);
