fd3852661632e5caaa9e64b8cbb19498dbde39b8
Ivonne Duran <111211850+ivonne-duran@users.noreply.github.com>
2025-04-08 23:05:33 -0600

    Componentes adicionales (#5)
    * Ajustes UI
    
    * Agregando rutas adicionales

--- SISTEC frontend/src/router.jsx
+++ SISTEC frontend/src/router.jsx
@@ -5,56 +5,74 @@ import LoginPage from "./pages/LoginPage";
 import About from "./components/About";
 import ServiciosTecnicos from "./pages/ServiciosPage"; // asegúrate de que esta ruta coincida
 import EmpleadoTecnico from "./components/EmpleadoTecnico/EmpleadoTecnico";
-import CategoriaSeleccionada from "./components/CategoriaSeleccionada/CategoriaSeleccionada"; 
+import CategoriaSeleccionada from "./components/CategoriaSeleccionada/CategoriaSeleccionada";
 import TecnicosDisponibles from "./components/TecnicosDisponibles/tecnicosDisponibles";
-import AsignarServicio from "./components/Tecnicos/AsignarServicio"; // asegúrate de que esta ruta coincida 
+import AsignarServicio from "./components/Tecnicos/AsignarServicio"; // asegúrate de que esta ruta coincida
 import AsignacionExitosa from "./components/Tecnicos/AsignacionExitosa"; // asegúrate de que esta ruta coincida
 import Diagnostico from "./components/Diagnostico";
 import AgregarSolicitudPage from "./pages/AgregarSolicitudPage";
+import Procesadores from "./components/Procesadores/Procesadores";
+import MovimientosComponente from "./components/MovimientosComponente/MovimientosComponentes";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />, 
+    path: "/",
+    element: <App />,
     children: [
       {
-        path: '/login',
-        element: <LoginPage />
+        path: "/login",
+        element: <LoginPage />,
       },
       {
-        path: '/acerca',
-        element: <About />
+        path: "/acerca",
+        element: <About />,
       },
       {
-        path: '/registro',
-        element: <RegisterPage />
+        path: "/registro",
+        element: <RegisterPage />,
       },
       {
-        path: '/servicios-tecnicos',
-        element: <ServiciosTecnicos />
+        path: "/servicios-tecnicos",
+        element: <ServiciosTecnicos />,
       },
       {
-        path: '/diagnostico',
-        element: <Diagnostico />
+        path: "/diagnostico",
+        element: <Diagnostico />,
       },
       {
-        path: '/agregar-solicitud',
-        element: <AgregarSolicitudPage />
-      },      
+        path: "/agregar-solicitud",
+        element: <AgregarSolicitudPage />,
+      },
       {
         path: "/tecnicos-disponibles",
         element: <TecnicosDisponibles />,
       },
       {
         path: "/asignar-tecnicos",
-        element: < AsignarServicio/>,
+        element: <AsignarServicio />,
       },
       {
         path: "/asignarcion-confirmada",
-        element: < AsignacionExitosa/>,
+        element: <AsignacionExitosa />,
       },
-    ]
-  }
+      {
+        path: "/Procesadores",
+        element: <Procesadores />,
+      },
+      {
+        path: "/MovimientosComponente",
+        element: <MovimientosComponente />,
+      },
+      {
+        path: "/empleado-tecnico",
+        element: <EmpleadoTecnico />,
+      },
+      {
+        path: "/categoria-seleccionada",
+        element: <CategoriaSeleccionada />,
+      },
+    ],
+  },
 ]);
 
 export default router;
